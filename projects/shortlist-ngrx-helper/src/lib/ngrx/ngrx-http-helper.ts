432bd01a9dd87fd92c688c5dce7476ccb1c876e6
Stefan Tirea <stefan.tirea.ext@arsinfo.de>
2022-04-14 23:29:05 +0200

    implement type safety instead of using any for custom actions

--- projects/shortlist-ngrx-helper/src/lib/ngrx/ngrx-http-helper.ts
+++ projects/shortlist-ngrx-helper/src/lib/ngrx/ngrx-http-helper.ts
@@ -1,6 +1,17 @@
-import {ActionCreator, createAction, createReducer, on, props} from '@ngrx/store';
+import {createAction, createReducer, on, props} from '@ngrx/store';
 import {LazyValue} from '../lazy-value';
-import {createNewState, ErrorActionProps, RequestActionProps, ResponseActionProps} from './ngrx-util';
+import {
+  createNewState,
+  ErrorAction,
+  ErrorActionCreator,
+  ErrorActionProps,
+  RequestActionCreator,
+  RequestActionProps,
+  ResetActionCreator,
+  ResponseAction,
+  ResponseActionCreator,
+  ResponseActionProps
+} from './ngrx-util';
 import {Actions, createEffect, CreateEffectMetadata, ofType} from '@ngrx/effects';
 import {catchError, map, mergeMap} from 'rxjs/operators';
 import {Observable, of} from 'rxjs';
@@ -9,23 +20,23 @@ import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 /**
  * Holds all possible required actions to call & handle a REST Endpoint
  */
-export interface HttpActions {
-  request: ActionCreator<any, any>,
-  response: ActionCreator<any, any>,
-  error: ActionCreator<any, any>,
-  reset: ActionCreator<any, any>,
+export interface HttpActions<REQ, RES> {
+  request: RequestActionCreator<REQ>,
+  response: ResponseActionCreator<REQ, RES>,
+  error: ErrorActionCreator<REQ>,
+  reset: ResetActionCreator
 }
 
-export const createHttpActions = <REQ, RES>(type: string): HttpActions => {
+export const createHttpActions = <REQ, RES>(type: string): HttpActions<REQ, RES> => {
   const request = createAction(type + ' (REQUEST)', props<RequestActionProps<REQ>>());
   const response = createAction(type + ' (RESPONSE)', props<ResponseActionProps<REQ, RES>>());
   const error = createAction(type + ' (ERROR)', props<ErrorActionProps<REQ>>());
   const reset = createAction(type + ' (RESET)');
-  return {request, response, error, reset} as HttpActions;
+  return {request, response, error, reset};
 };
 
 
-export const createHttpReducer = <S, K extends keyof S>(actions: HttpActions, initState: S, fieldName: K) => {
+export const createHttpReducer = <S, K extends keyof S>(actions: HttpActions<any, any>, initState: S, fieldName: K) => {
   return createReducer(
     initState,
     // @ts-ignore
@@ -35,17 +46,21 @@ export const createHttpReducer = <S, K extends keyof S>(actions: HttpActions, in
     // @ts-ignore
     on(actions.error, (state: S, error: ErrorActionProps<any>) => (createNewState(state, fieldName, LazyValue.error(error.httpError)))),
     // @ts-ignore
-    on(actions.reset, (state: S) => (createNewState(state, fieldName, LazyValue.loading()))),
+    on(actions.reset, (state: S) => (createNewState(state, fieldName, undefined))),
   );
 };
 
-export const createHttpEffect = <REQ, RES>(actions$: Actions, myAction: HttpActions, fn: (payload: REQ | undefined) => Observable<HttpResponse<RES>>): Observable<HttpResponse<RES>> & CreateEffectMetadata => {
-  return createEffect(() => actions$.pipe(
-      ofType(myAction.request),
+export const createHttpEffect = <REQ, RES>(allActions$: Actions, httpActions: HttpActions<REQ, RES>, fn: (payload: REQ | undefined) => Observable<HttpResponse<RES>>): Observable<ResponseAction<REQ, RES> | ErrorAction<REQ>> & CreateEffectMetadata => {
+  return createEffect(() => allActions$.pipe(
+      ofType(httpActions.request),
       mergeMap((payload: RequestActionProps<REQ>) => fn(payload.payload)
         .pipe(
-          map(httpResponse => myAction.response({payload: httpResponse.body, requestPayload: payload.payload})),
-          catchError((err: HttpErrorResponse) => of(myAction.error({requestPayload: payload.payload, httpStatus: err.status, httpError: err})))
+          map(httpResponse => httpActions.response({payload: httpResponse.body, requestPayload: payload.payload})),
+          catchError((err: HttpErrorResponse) => of(httpActions.error({
+            requestPayload: payload.payload,
+            httpStatus: err.status,
+            httpError: err
+          })))
         ))
     )
   );
--- projects/shortlist-ngrx-helper/src/lib/ngrx/ngrx-util.ts
+++ projects/shortlist-ngrx-helper/src/lib/ngrx/ngrx-util.ts
@@ -1,6 +1,7 @@
 import {HttpErrorResponse} from '@angular/common/http';
 import {LazyValue} from '../lazy-value';
-import {Action, ActionReducer} from '@ngrx/store';
+import {Action, ActionCreator, ActionReducer} from '@ngrx/store';
+import {TypedAction} from "@ngrx/store/src/models";
 
 export interface RequestActionProps<REQ> {
   payload?: REQ;
@@ -17,6 +18,17 @@ export interface ErrorActionProps<REQ> {
   httpError?: HttpErrorResponse;
 }
 
+// Custom Action types for autocomplete when dispatching actions => See ngrx createAction(...) return type
+export type RequestAction<REQ> = RequestActionProps<REQ> & TypedAction<string>;
+export type ResponseAction<REQ, RES> = ResponseActionProps<REQ, RES> & TypedAction<string>;
+export type ErrorAction<REQ> = ErrorActionProps<REQ> & TypedAction<string>;
+
+// Custom ActionCreator combines empty ActionCreator with Payload Type
+export type RequestActionCreator<REQ> = ActionCreator<string, (props: RequestActionProps<REQ>) => RequestAction<REQ>>;
+export type ResponseActionCreator<REQ, RES> = ActionCreator<string, (props: ResponseActionProps<REQ, RES>) => ResponseAction<REQ, RES>>;
+export type ErrorActionCreator<REQ> = ActionCreator<string, (props: ErrorActionProps<REQ>) => ErrorAction<REQ>>;
+export type ResetActionCreator = ActionCreator<string, () => TypedAction<string>>;
+
 export const createNewState = <S, K extends keyof S>(state: S, fieldName: K, value: LazyValue<S[K]>): S => {
   const newState = {...state};
   // @ts-ignore
@@ -26,8 +38,6 @@ export const createNewState = <S, K extends keyof S>(state: S, fieldName: K, val
 
 export const combineHelperReducers = <S>(...reducers: ActionReducer<S>[]): (state: S, action: Action) => S => (state: S, action: Action) => {
   let newState = state;
-  reducers.forEach(item => {
-    newState = item(newState, action);
-  });
+  reducers.forEach(item => newState = item(newState, action));
   return newState;
 };
